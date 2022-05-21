d14632e63771556cd24aafc7a39a39cc63c4150c
Minder Colin Joshua <colinjoshua.minder@students.bfh.ch>
2022-05-21 14:32:25 +0200

    code structure changed; works

--- env.py
+++ env.py
@@ -12,18 +12,9 @@ import random
 [5]: https://www.castrads.com/uk/resources/calculators/panel-radiator-outputs/
 '''
 
-class HeatObject():
-    def take():
-        pass
-    def give():
-        pass
-    def energyniveau():
-        # give = +  take = -
-        pass
 
 
-
-class Radiator(HeatObject):
+class Radiator():
     '''
         This class represents everything related to the radiator. Initially, the
         radiator has a hight and lenth. It is assumed, that this is a typical radiator 
@@ -32,31 +23,34 @@ class Radiator(HeatObject):
         cooling down. 
     '''
 
-    def give():
-        # radiates heat
-        pass
-
     def __init__(self, length=1, hight=0.5):
         self.radiator_length = length # in meter [2]
         self.radiator_hight = hight # in meter [2]
-        self.radiator_state = 0 # Continuous, between 0 and 1
+        self.state = 0 # Continuous, between 0 and 1
 
-    def update_state(self, action):
+    def give(self, radiator_lenght, radiator_hight):
+        ''' This method calculates the rate of heat change in the zone. The value gets multiplied 
+            by the ctrl_value. This variable is defined by the choosen action divided by 5, which 
+            is the maximum value on a common thermostatic valve [2]. '''
+        give = self.state * (41 * 3.1 * radiator_lenght * (1 + 8 * radiator_hight))
+        return give
+
+    def set_state(self, action):
         ''' Method that returns the current state of the radiator. The return value is 
             continuous between 0 and 1. This means that with the value 1 the radiator 
             reached its maximum power. '''
         if action != 0:
-            self.radiator_state += action/5 * 0.0416667 # 0.0416667 is the gain per second at the highest level (5)
-            if self.radiator_state >= 1:
-                self.radiator_state = 1
+            self.state += action/5 * 0.0416667 # 0.0416667 is the gain per second at the highest level (5)
+            if self.state >= 1:
+                self.state = 1
         else:
-            self.radiator_state += (-3/5) * 0.0416667 # -3 is an assumption
-            if self.radiator_state <= 0:
-                self.radiator_state = 0
+            self.state += (-3/5) * 0.0416667 # -3 is an assumption
+            if self.state <= 0:
+                self.state = 0
 
 
 
-class RoomEnv(HeatObject):
+class RoomEnv():
     '''
         This class represents the environment in which the agent will train.
         It is built up on the YouTube tutorial Building a Custom Environment for 
@@ -72,7 +66,6 @@ class RoomEnv(HeatObject):
             - y_dim: Room width in meter
             - z_dim: Room hight in meter
         '''
-        Radiator.__init__(self)
         self.action_space = Discrete(6)
         self.observation_space = Box(low=np.array([0]), high=np.array([100]))
         self.temp_low = temp_low
@@ -94,36 +87,28 @@ class RoomEnv(HeatObject):
         pass
 
     @staticmethod
-    def roc_heat_in_walls(heat_trans_coef, x_dim, y_dim, z_dim, ambient_temp, zone_temp):
+    def take(heat_trans_coef, x_dim, y_dim, z_dim, ambient_temp, zone_temp):
         ''' This method calculates the rate of heat change in the walls. It is assumed, 
             that all walls (excluding the floor) have the same values [1]. '''
         area_a = x_dim * z_dim * 2
         area_b = y_dim * z_dim * 2
         area_c = x_dim * y_dim * 1
         total_area = area_a + area_b + area_c
-        Hwzt = heat_trans_coef * total_area * (ambient_temp - zone_temp)
-        return Hwzt
+        take = heat_trans_coef * total_area * (ambient_temp - zone_temp)
+        return take
+
 
     @staticmethod
-    def roc_heat_in_zone(radiator_lenght, radiator_hight, radiator_state):
-        ''' This method calculates the rate of heat change in the zone. The value gets multiplied 
-            by the ctrl_value. This variable is defined by the choosen action divided by 5, which 
-            is the maximum value on a common thermostatic valve [2]. '''
-        Hhzt = radiator_state * (41 * 3.1 * radiator_lenght * (1 + 8 * radiator_hight))
-        return Hhzt
-
-    @staticmethod
-    def zone_temp(delta_t, Hwzt, Hhzt, room_volume, air_density, heat_of_air):
+    def cal_delta_temp(delta_t, Hwzt, Hhzt, room_volume, air_density, heat_of_air):
         ''' This method calculates the temperature difference between t and t+1 in the zone [1]. '''
         delta_temp = delta_t * (Hwzt + Hhzt) / room_volume * air_density * heat_of_air
         return delta_temp
 
-    def step(self, action):
+    def step(self):
         ''' Given an action, this method performs the change in the environment and returns state, reward, done and info. '''
-        self.update_state(action)
-        Hwzt = self.roc_heat_in_walls(self.heat_trans_coef, self.x_dim, self.y_dim, self.z_dim, 0, 20)
-        Hhzt = self.roc_heat_in_zone(self.radiator_length, self.radiator_hight, self.radiator_state)
-        delta_temp = self.zone_temp(self.delta_t, Hwzt, Hhzt, self.room_volume, self.air_density, self.heat_of_air)
+        room_take = self.take(self.heat_trans_coef, self.x_dim, self.y_dim, self.z_dim, 0, 20)
+        rad_give = raditator.give(raditator.radiator_length, raditator.radiator_hight)
+        delta_temp = self.cal_delta_temp(self.delta_t, room_take, rad_give, self.room_volume, self.air_density, self.heat_of_air)
         self.state += delta_temp
         return self.state
 
@@ -135,12 +120,12 @@ class RoomEnv(HeatObject):
         self.state = ((self.temp_low + self.temp_up) / 2) + random.randint(-3,3)
         self.sim_duration = 100 # TODO: Placeholder - must be defined later
 
-env = RoomEnv()
-rad = Radiator()
+room = RoomEnv()
+raditator = Radiator()
 
 episode = 1
 for episode in range(1,episode+1):
-    state = env.reset()
+    state = room.reset()
     done = False
     score = 0
     time_step = 0
@@ -148,18 +133,19 @@ for episode in range(1,episode+1):
 
     while not done:
         if time_step % 60 == 0:
-            action = env.action_space.sample()
-            rad.setCurrentAction(action)
-            print("action: ", action, "state: ", env.step(action), "rad_stae: ", env.radiator_state)
+            action = room.action_space.sample()
+            raditator.set_state(action)
+            print("action: ", action, "state: ", room.step(), "rad_statee: ", raditator.state)
             #n_state, reward, done, info = env.step(action)
         else:
-            print("action: ", action, "state: ", env.step(action), "rad_stae: ", env.radiator_state)
+            raditator.set_state(action)
+            print("action: ", action, "state: ", room.step(), "rad_state: ", raditator.state)
             #n_state, reward, done, info = env.step(action)
-        env.influences(rad)
+        #room.influences(raditator)
         #score+=reward
         time_step += 1
-        env.step()
-        rad.update()
+        #room.step()
+        #raditator.update()
         #window.update()
 
         # For debugging
